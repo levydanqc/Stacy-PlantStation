1133746fa255a061ba1755972a2a951c3f04ae81
Dan Levy <danlevy.ca@icloud.com>
2025-05-03 00:16:32 -0400

    Adding calibration of capacitive sensor

--- PlatformIO/src/calibration.cpp
+++ PlatformIO/src/calibration.cpp
@@ -1,38 +1,38 @@
-// #include "configuration.h"
+#include "configuration.h"
 
-// void calibrate();
-// float readCapacitance();
+void calibrate();
+float readCapacitance();
 
-// void setup() {
-//   Serial.begin(115200);
-//   Serial.println("\nESP32 in Calibration Mode!");
-// }
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\nESP32 in Calibration Mode!");
+}
 
-// void loop() {
-//   calibrate();
+void loop() {
+  calibrate();
 
-//   delay(1000);
-// }
+  delay(1000);
+}
 
-// /**
-//  * @brief Calibrates the capacitance sensor by taking readings in known
-//  * environments.
-//  * The calibration values are stored in EEPROM.
-//  **/
-// void calibrate() {
-//   uint16_t sensorValue = analogRead(CAPACITANCE_PIN);
+/**
+ * @brief Calibrates the capacitance sensor by taking readings in known
+ * environments.
+ * The calibration values are stored in EEPROM.
+ **/
+void calibrate() {
+  uint16_t sensorValue = analogRead(CAPACITANCE_PIN);
 
-//   for (int i = 0; i < 5; i++) {
-//     sensorValue = (sensorValue + analogRead(CAPACITANCE_PIN)) / 2;
-//   }
+  for (int i = 0; i < 5; i++) {
+    sensorValue = (sensorValue + analogRead(CAPACITANCE_PIN)) / 2;
+  }
 
-//   float voltage = sensorValue * (3.3 / 4095.0);
-//   float capacitance = (voltage * 10.0) + 5.0;
+  float voltage = sensorValue * (3.3 / 4095.0);
+  float capacitance = (voltage * 10.0) + 5.0;
 
-//   Serial.print("Sensor Value: ");
-//   Serial.println(sensorValue);
-//   Serial.print("Capacitance: ");
-//   Serial.println(capacitance);
-//   Serial.print("Voltage: ");
-//   Serial.println(voltage);
-// }
+  Serial.print("Sensor Value: ");
+  Serial.println(sensorValue);
+  Serial.print("Capacitance: ");
+  Serial.println(capacitance);
+  Serial.print("Voltage: ");
+  Serial.println(voltage);
+}
