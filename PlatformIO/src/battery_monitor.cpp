0aa9277a5185a9e3cafcaaf8a09d048397ed9535
Dan Levy <danlevy.ca@icloud.com>
2025-06-15 22:55:45 -0400

    Added some example code for battery voltage

--- PlatformIO/src/battery_monitor.cpp
+++ PlatformIO/src/battery_monitor.cpp
@@ -8,6 +8,12 @@
  * @return The battery voltage.
  */
 float BatteryMonitor::readBatteryVoltage() {
+  //   uint32_t Vbatt = 0;
+  // for(int i = 0; i < 16; i++) {
+  //   Vbatt = Vbatt + analogReadMilliVolts(A0); // ADC with correction   
+  // }
+  // float Vbattf = 2 * Vbatt / 16 / 1000.0;     // attenuation ratio 1/2, mV --> V
+
   analogSetPinAttenuation(BATTERY_PIN,
                           ADC_11db);      // Configure ADC for 0-2.5V range
   uint16_t raw = analogRead(BATTERY_PIN); // Read raw ADC value
