ef847d3ad2f548bb591784d3f10860e3e0866390
Dan Levy <danlevy.ca@icloud.com>
2025-06-24 14:23:04 -0400

    cleanup bme280

--- PlatformIO/include/sensor_handler.h
+++ PlatformIO/include/sensor_handler.h
@@ -2,12 +2,10 @@
 #define SENSOR_HANDLER_H
 
 #include "configuration.h"
-// #include <Adafruit_BME280.h>
 #include <Adafruit_HDC302x.h>
 
 class SensorHandler {
 private:
-  // static Adafruit_BME280 bme280;
   static Adafruit_HDC302x hdc3022;
   static void readHDC(SensorData &sensorData);
   static float getMoisture();
