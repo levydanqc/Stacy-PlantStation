f4b271a498de9a766e0e3e31cbda4b2a1ae094f4
Dan Levy <danlevy.ca@icloud.com>
2025-07-09 19:37:44 -0400

    Added refresh-token routes

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const verifyToken = function (req, res, next) {
   const JWT_SECRET = process.env.JWT_SECRET;
 
+  console.log('Trying to access protected route:', req.originalUrl);
+
   const authHeader = req.headers['authorization'];
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     console.warn('Unauthorized: No Authorization header provided');
@@ -15,14 +17,18 @@ const verifyToken = function (req, res, next) {
 
   console.log('Received token:', token);
 
-  if (!token) return res.status(401).json({ error: 'Token missing' });
+  if (!token) {
+    console.warn('Unauthorized: Token missing');
+    return res.status(401).json({ error: 'Token missing' });
+  }
 
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
+      console.error('Token verification failed:', err);
       return res.status(403).json({ error: 'Token invalid or expired' });
     }
 
-    // Attach decoded data to request
+    console.log('Token verified successfully:', decoded);
     req.device = decoded;
     next();
   });
